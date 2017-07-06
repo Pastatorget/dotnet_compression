c48ad56cd21cb5629ad36dfb65c03dbd26005b4e
asabla <sparven89@gmail.com>
2017-07-06 20:16:27 +0200

    Initial commit for deflate and gzip attribute support

---
+++ src/dotnet_compression/GzipAttribute.cs
@@ -0,0 +1,57 @@
+﻿using dotnet_compression.Resources;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace dotnet_compression
+{
+    public class GzipAttribute : ActionFilterAttribute
+    {
+        public CompressionLevel CompressionLevel { get; set; } = CompressionLevel.Fastest;
+        public Dictionary<string, string> Headers { get; set; }
+
+        public override void OnActionExecuted(HttpActionExecutedContext context)
+        {
+            var content = new byte[0];
+
+            if (context.Response.Content != null)
+            {
+                var bytes = context.Response.Content.ReadAsByteArrayAsync().Result;
+
+                if (bytes != null)
+                {
+                    content = CompressionHelper.GzipBytes(bytes, this.CompressionLevel);
+                }
+            }
+
+            context.Response.Content = new ByteArrayContent(content);
+
+            if (this.Headers.Count > 0)
+            {
+                // Removes all header key values matching in this.Headers
+                foreach (var header in this.Headers)
+                {
+                    if (context.Response.Content.Headers.Contains(header.Key))
+                    {
+                        // Removes header based on key
+                        context.Response.Content.Headers.Remove(header.Key);
+
+                        // Add updated header key and value
+                        context.Response.Content.Headers.Add(header.Key, header.Value);
+                    }
+                }
+            }
+            else
+            {
+                context.Response.Content.Headers.Remove("Content-Type");
+                context.Response.Content.Headers.Add("Content-Type", "application/json");
+            }
+
+            // For now we're forcing Content-encoding to gzip, since that's the compression method used
+            context.Response.Content.Headers.Add("Content-encoding", "gzip");
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ src/dotnet_compression/Resources/CompressionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace dotnet_compression.Resources
+{
+    public class CompressionHelper
+    {
+        public static byte[] DeflateBytes(byte[] str, CompressionLevel compressionLevel = CompressionLevel.Fastest)
+        {
+            using (var output = new MemoryStream())
+            {
+                using (var compressor = new DeflateStream(output, compressionLevel))
+                {
+                    compressor.Write(str, 0, str.Length);
+                }
+
+                return output.ToArray();
+            }
+        }
+
+        public static byte[] GzipBytes(byte[] str, CompressionLevel compressionLevel = CompressionLevel.Fastest)
+        {
+            using (var output = new MemoryStream())
+            {
+                using (var compressor = new GZipStream(output, compressionLevel))
+                {
+                    compressor.Write(str, 0, str.Length);
+                }
+
+                return output.ToArray();
+            }
+        }
+    }
+}
